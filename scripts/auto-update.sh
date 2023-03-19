336bb28cba4a3d3509b45f452558569604eaa2f4
Zane on Lark <info+lark@zane.it>
2023-03-20 00:06:44 +0100

    fix autoupdate

--- scripts/auto-update.sh
+++ scripts/auto-update.sh
@@ -2,5 +2,5 @@
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/auto-update.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
-source "${WEBSTACKUP_SCRIPT_DIR}filesystem/auto-update.sh" "${PROJECT_DIR}"
+bash "${WEBSTACKUP_SCRIPT_DIR}filesystem/auto-update.sh" "${PROJECT_DIR}"
 source "${SCRIPT_DIR}script_end.sh"
