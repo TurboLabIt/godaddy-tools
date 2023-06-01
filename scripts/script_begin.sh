c7740989be6caa6906eeb6dcc89058a6db93e22c
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2023-06-01 16:37:37 +0200

    fix emoji

--- scripts/script_begin.sh
+++ scripts/script_begin.sh
@@ -5,11 +5,11 @@ APP_NAME="godaddy-tools"
 PROJECT_FRAMEWORK=symfony
 
 ## https://github.com/TurboLabIt/webstackup/blob/master/script/filesystem/script_begin_start.sh
-source "/usr/local/turbolab.it/webstackup/script/filesystem/script_begin_start.sh" 
+source "/usr/local/turbolab.it/webstackup/script/filesystem/script_begin_start.sh"
 
 ZZ_CMD_SUFFIX=1
 
 ## Enviroment variables and checks
 if [ "$APP_ENV" = "prod" ]; then
-  EMOJI=🔧
+  EMOJI=wrench
 fi
